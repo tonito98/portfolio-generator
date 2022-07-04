d57e95bc9a650f0c5905585ea59800e6536e1321
tonito98 <86890715+tonito98@users.noreply.github.com>
2022-07-04 19:37:06 +0200

    added inquire.prompt to promptu user for their name

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
+import inquirer from 'inquirer';
+console.log(inquirer);
+// const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));

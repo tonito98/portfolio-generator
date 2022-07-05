3d91cd847768bb1b15b4f070802df5ebc0c21d53
tonito98 <86890715+tonito98@users.noreply.github.com>
2022-07-05 18:57:28 +0200

    Validate answer before moving on to the next question

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ function promptUser() {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                 } else {
+                    console.log('Please enter your name!');
+                    return false;
+                 }
+                }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                 } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                 }
+                }
         },
         {
             type: 'input',
@@ -37,18 +53,48 @@ Add a New Project
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                 } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                 }
+                }
+           
         },
         {
             type:'input',
             name: 'description',
-            message: 'Provide a description of your project (Required)'
+            message: 'Provide a description of your project (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                 } else {
+                    console.log('Please enter your project description!');
+                    return false;
+                 }
+                }
         },
         {
             type: 'checkbox',
             name: 'languages',
             message: 'What did you build this project with? (Check all that apply)',
             choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                 } else {
+                    console.log('Please enter your GitHub link!');
+                    return false;
+                 }
+                }
         },
         {
             type: 'confirm',
